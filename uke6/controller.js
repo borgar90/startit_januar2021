c5dd934b45e0e86f07b076134104cf87b0e6f7ad
borgar90 <73884383+borgar90@users.noreply.github.com>
2021-02-09 12:47:24 +0100

    Update controller.js

--- uke6/controller.js
+++ uke6/controller.js
@@ -1,21 +1,20 @@
-function selectBar(element){
+function selectBar(element){ // funksjon for å velge stolpe
    
     style = "stroke-width:1;stroke:rgb(0,0,0);" //gjør klar style variablen. 
     if(index != element){ //selected a bar
-        index = element ;
-        disabled = "";
-        valgtStolpe = element;
+        index = element ; // setter indexvariabelen til element. Klikker man på samme baren er index = element, og vi nullstiller
+        disabled = ""; // slår av disabled på knappene
+        valgtStolpe = element; //opdaterer valgt stolpe teksten med valgt stolpe
         
            show()
-        return index
+        return index // returnerer index når bar er valgt.
     }else{ //deselected bar
-        style = "";
-        index = null;
-        disabled = "disabled";
-        valgtStolpe = "ingen";
-        
-       show()
-       return false
+        style = ""; //slår av style
+        index = null; //ingen bar er selected så index skal være null.
+        disabled = "disabled"; //slår av knappene
+        valgtStolpe = "ingen"; //oppdaterer teksten til ingen stolpe valgt. 
+       show() // refresher viewet
+       return false // til unittesting, returnerer false når ingen bar er valgt
     }
      
 }
